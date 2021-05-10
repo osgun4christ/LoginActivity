bf3c639fd68e63dcf8564e828581765e69f8164b
osgun4christ <osgun4christ@gmail.com>
2021-05-10 22:52:57 +0000

    1st commit

--- app/src/main/java/com/example/loginactivity/LoginActivity.kt
+++ app/src/main/java/com/example/loginactivity/LoginActivity.kt
@@ -3,7 +3,7 @@ package com.example.loginactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class activity_login : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
--- app/src/main/java/com/example/loginactivity/MainActivity.kt
+++ app/src/main/java/com/example/loginactivity/MainActivity.kt
@@ -1,28 +1,55 @@
 package com.example.loginactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.loginactivity.databinding.ActivityMainBinding
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
-    var binding:ActivityMainBinding? = null
+    var binding: ActivityMainBinding? = null
     var adapter: FactAdapter? = null
+        override fun onCreate(saveInstanceState:Bundle?) {
+            super.onCreate(saveInstanceState)
+            setContentView(R.layout.activity_main)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding?.root)
-        setUpFact()
+            signIn()
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding?.root)
+            setUpFact()
+        }
+
+    private fun setUpFact() {
+            adapter = FactAdapter( this, DummyData.funFacts)
+            binding?.factListView?.adapter = adapter
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+            binding = null
     }
 
-    private fun setUpFact(){
-        adapter = FactAdapter( this, DummyData.funFacts)
-        binding?.factListView?.adapter = adapter
-    }
+    private fun signIn() {
+            val email = findViewById<TextInputEditText>(R.id.email_edit_text)
+            val password = findViewById<TextInputEditText>(R.id.password_edit_text)
+            val signinButton = findViewById<MaterialButton>(R.id.next_button)
 
-    override fun onDestroy() {
-        super.onDestroy()
-        binding = null
-    }
+            signinButton.setOnClickListener() {
+                if (email.toString().isNullOrEmpty() || password.toString().isNullOrEmpty()) {
+                    Toast.makeText(this, "Email or password is not provided", Toast.LENGTH_LONG).show()
+                } else {
+                    if (email.toString() == "osgun4christ@gmail.com" && password.toString() == "iLoveJESUS") {
+                        val intent = Intent( this, LoginActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "Email or password is not provided", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
 }
+
